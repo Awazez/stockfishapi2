6659c995f90dbc8231bc86c22eb030848e61f62f
cherbourg@fub.fr <cherbourg@fub.fr>
2024-09-09 15:36:08 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { spawn } = require('child_process');
+const app = express();
+const port = 3000;
+
+// Function to send commands to Stockfish
+function runStockfish(command) {
+  return new Promise((resolve, reject) => {
+    const stockfish = spawn('stockfish');
+    let output = '';
+
+    stockfish.stdout.on('data', (data) => {
+      output += data.toString();
+    });
+
+    stockfish.stderr.on('data', (data) => {
+      reject(data.toString());
+    });
+
+    stockfish.on('close', (code) => {
+      resolve(output);
+    });
+
+    stockfish.stdin.write(command + '\n');
+    stockfish.stdin.write('quit\n');
+  });
+}
+
+// API to evaluate a position
+app.get('/evaluate', async (req, res) => {
+  const fen = req.query.fen; // FEN position from query parameters
+  if (!fen) {
+    return res.status(400).send('FEN parameter is required');
+  }
+
+  try {
+    const stockfishOutput = await runStockfish(`position fen ${fen}\ngo`);
+    res.send(stockfishOutput);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Stockfish API listening at http://localhost:${port}`);
+});
