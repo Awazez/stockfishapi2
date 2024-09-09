7902185abec8dff1f4c219b81a8f22335a96468a
cherbourg@fub.fr <cherbourg@fub.fr>
2024-09-09 16:02:25 +0200

    add multipv

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 // Function to send commands to Stockfish
-function runStockfish(command) {
+function runStockfish(command, multipv = 1) {
   return new Promise((resolve, reject) => {
     const stockfish = spawn('stockfish');
     let output = '';
@@ -21,20 +21,25 @@ function runStockfish(command) {
       resolve(output);
     });
 
+    // Set the number of lines to calculate (MultiPV)
+    stockfish.stdin.write(`setoption name MultiPV value ${multipv}\n`);
     stockfish.stdin.write(command + '\n');
+    stockfish.stdin.write('go\n');
     stockfish.stdin.write('quit\n');
   });
 }
 
-// API to evaluate a position
+// API to evaluate a position with multiple lines
 app.get('/evaluate', async (req, res) => {
   const fen = req.query.fen; // FEN position from query parameters
+  const multipv = req.query.multipv || 1; // Number of lines (MultiPV)
+  
   if (!fen) {
     return res.status(400).send('FEN parameter is required');
   }
 
   try {
-    const stockfishOutput = await runStockfish(`position fen ${fen}\ngo`);
+    const stockfishOutput = await runStockfish(`position fen ${fen}`, multipv);
     res.send(stockfishOutput);
   } catch (error) {
     res.status(500).send(error);
@@ -44,3 +49,5 @@ app.get('/evaluate', async (req, res) => {
 app.listen(port, () => {
   console.log(`Stockfish API listening at http://localhost:${port}`);
 });
+
+
